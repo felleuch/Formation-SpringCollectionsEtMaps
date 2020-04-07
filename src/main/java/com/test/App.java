f2b8aa77934cd04d27d74a89add166725efc01b1
ELLEUCH <ELLEUCH@DESKTOP-E28062K>
2020-04-07 10:05:20 +0200

    init project

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"SpringBeans.xml");
+
+		Customer cust = (Customer) context.getBean("CustomerBean");
+		System.out.println(cust);
+		
+		
+		
+		Client client = (Client) context.getBean("clientBean");
+		
+		System.out.println("Liste des commandes du client :"+client.getName());
+		
+		for(String elem:client.getOrders()) {
+			System.out.println(elem);
+		}
+		
+		
+		
+	}
+}
---
+++ src/main/java/com/test/Client.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import java.util.List;
+
+public class Client {
+	
+	
+	String name;
+	
+	String matricule;
+
+	List<String> orders;
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMatricule() {
+		return matricule;
+	}
+
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	public List<String> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<String> orders) {
+		this.orders = orders;
+	}
+	
+	
+	
+	
+
+}
